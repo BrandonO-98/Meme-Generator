2339bf6d027cd6776c0ad77e4d0fc281b23cc198
BrandonO-98 <brandonshew98@gmail.com>
2022-04-21 18:25:26 -0400

    Completed meme-generator with api call

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <div className='header-container'>
+      <img src='/images/meme-face.png' className='meme-face' alt='Meme Face' />
+      <h1 className='meme-title'>Meme Generator</h1>
+      <h2 className='react-course'>React Course - Project 2</h2>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faImage} from "@fortawesome/free-solid-svg-icons";
+
+function Main() {
+  const [allMemes, setAllMemes] = React.useState([]);
+
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "",
+  });
+
+  React.useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((data) => setAllMemes(data.data.memes));
+  }, []);
+
+  function getMeme() {
+    const randNumb = Math.floor(Math.random() * allMemes.length);
+    const url = allMemes[randNumb].url;
+    setMeme((prevState) => ({...prevState, randomImage: url}));
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target;
+    setMeme((prevState) => ({...prevState, [name]: value}));
+  }
+
+  return (
+    <main>
+      <div className='main-container'>
+        <div className='form'>
+          <input
+            type='text'
+            className='form-input'
+            placeholder='Top Text'
+            value={meme.topText}
+            name='topText'
+            onChange={handleChange}
+          />
+          <input
+            type='text'
+            className='form-input'
+            placeholder='Bottom Text'
+            value={meme.bottomText}
+            name='bottomText'
+            onChange={handleChange}
+          />
+          <button className='form-button' onClick={getMeme}>
+            Get a new meme image
+            <FontAwesomeIcon icon={faImage} className='icon' />
+          </button>
+        </div>
+        <div className='meme'>
+          <img src={meme.randomImage} className='meme-image' />
+          <h2 className='meme-text-top'>{meme.topText}</h2>
+          <h2 className='meme-text-bottom'>{meme.bottomText}</h2>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
